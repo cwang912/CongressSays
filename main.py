825dc382dd6ce3c9d7f3c642e85d823c568e9526
cwang912 <cwang912@gmail.com>
2014-04-01 16:01:47 -0400

    Add files
    Initial file dump

---
+++ main.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import webapp2
+import tweepy
+import urllib2
+import urllib
+from datetime import date, timedelta
+import json
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+    	day = date.today() - timedelta(1)
+    	endpoint = 'http://capitolwords.org/api/phrases.json' 
+    	query_params = {'apikey': 'eea337bef84a4129ba8639825c1b39ac', 'entity_type': 'date', 'entity_value': day, 'n': 1}
+    	formatted_args = urllib.urlencode(query_params)
+    	page = urllib2.urlopen(url = endpoint + "?" + formatted_args)
+    	words = json.loads(page.read())
+    	msg = 'The top words from Congress yesterday were '
+    	for i in words[0:3]:
+    		msg = msg + '"' + i['ngram']  + '", '
+    	msg = msg + 'and "' + words[3]['ngram'] + '."'
+    	self.response.write(msg)
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
+
+def main():
+    app.run()
